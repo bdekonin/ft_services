d47c5339439ce8c36896496759450d6a66a780c0
bdekonin <bdekonin@student.codam.nl>
2021-01-26 11:42:14 +0100

    .

--- start.sh
+++ start.sh
@@ -4,22 +4,32 @@ SSH_USER="admin" # = default username for ssh
 SSH_PASSWORD="admin" # default password for ssh
 
 
-printf "✅  Starting minikube\n"
 minikube delete
+printf "✅  Starting minikube\n"
 minikube start --vm-driver=virtualbox
 minikube addons enable metallb
 minikube addons enable dashboard
 
-
 # Setting up Minikube's Docker
-
 eval $(minikube docker-env)
 
-
 # Configuring metallb:
-
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-kubectl apply -f srcs/metallb/metallb.yaml
+if [[ $(hostname) == "Bob's MacBook Pro" ]];
+then
+	ACCESS_IP="192.168.99.100"
+	kubectl apply -f srcs/metallb/school_metallb.yaml
+else
+	if [[ $(ipconfig getifaddr en0) == "172.20.10.2" ]];
+	then
+		ACCESS_IP="172.20.10.100"
+		kubectl apply -f srcs/metallb/work_metallb.yaml
+	else
+		ACCESS_IP="192.168.64.100"
+		kubectl apply -f srcs/metallb/home_metallb.yaml
+	fi
+fi
+
 
 
 # Creating an serviceaccount:
@@ -38,19 +48,18 @@ docker build -t influxdb srcs/influxdb
 kubectl apply -f srcs/influxdb.yaml
 
 
-
 # Building Nginx
 docker build -t nginx srcs/nginx
 kubectl apply -f srcs/nginx.yaml
 
 
 # Building PhpMyAdmin
-docker build -t phpmyadmin srcs/phpmyadmin
+docker build -t phpmyadmin srcs/phpmyadmin > /dev/null
 kubectl apply -f srcs/phpmyadmin.yaml
 
 
 # Building Wordpress
-docker build -t wordpress srcs/wordpress
+docker build -t wordpress srcs/wordpress > /dev/null
 kubectl apply -f srcs/wordpress.yaml
 
 
@@ -60,16 +69,16 @@ kubectl apply -f srcs/telegraf.yaml
 
 
 # Building Grafana
-docker build -t grafana srcs/grafana
+docker build -t grafana srcs/grafana > /dev/null
 kubectl apply -f srcs/grafana.yaml
 
 
 echo "--------------------------------------------------------------------------------
 Service:
-	WordPress:	192.168.99.100:5050
+	WordPress:	$ACCESS_IP:5050
 		credentials: [admin]-[password]
-	PhpMyAdmin: 192.168.99.100:5000
+	PhpMyAdmin: $ACCESS_IP:5000
 		credentials: [root]-[password]
-	Grafana: 192.168.99.100:3000
+	Grafana: $ACCESS_IP:3000
 		credentials: [xxx]-[xxx]
 "
